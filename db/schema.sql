619832466aad3197ab387ba21edb71e55563b3d1
Alex Wilkinson <mrwilkinson1@gmail.com>
2022-08-23 17:01:40 -0400

    schema added

--- db/schema.sql
+++ db/schema.sql
@@ -3,14 +3,25 @@ CREATE DATABASE business_db;
 
 
 USE business_db;
-CREATE TABLE department
+CREATE TABLE department(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
 
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+FOREIGN KEY (department_id) REFERENCES department(id)
+);
 
-
-
-CREATE TABLE role
-
-
-
-
-CREATE TABLE employee
+CREATE TABLE employee (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+last_name VARCHAR(30),
+first_name VARCHAR(30),
+manager_id INT,
+role_id INT,
+FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,19 +1,22 @@
-INSERT INTO departments (name,id)
-VALUE   (1, "Senior Mgmt")
-        (2, "Engineering")
-        (3, "Sales")
-        (4, "Accounting")
-        (5,"HR")
+INSERT INTO departments (name)
+VALUE   ( "Senior Mgmt"),
+        ( "Engineering"),
+        ( "Sales"),
+        ( "Accounting"),
+        ( "HR");
 
-INSERT INTO
-VALUES  (1, "CEO")
-        (2, "CFO")
-        (2, "Senior Engineer")
-        (3, "Jr Engineer")
-        (3, "Sales person")
-        (4, "HR Mgmt")
-        (5, "Accountant")
-INSERT INTO emplyee (Last_name, First_name, mgmt_id, role_ id)
-VALUES  ()
-        ()
+INSERT INTO (department_id, title, salary)
+VALUES  (1, "CEO", 250000),
+        (1, "CFO", 200000),
+        (2, "Senior Engineer", 175000),
+        (3, "Salesman", 80000),
+        (4, "HR Mgmt", 60000),
+        (4, "Accountant", 80000);
 
+INSERT INTO emplyee (Last_name, First_name, manager_id, role_ id)
+VALUES  ('Cheerio', 'Byron', 1, 1),
+        ('Niteowl', 'Hugo', 2, 3),
+        ('Smith', 'Velvet', 3, 2),
+        ('Slurch', 'Megan', 4, NULL),
+        ('Li', 'Kemp', 5, NULL),
+        ('Pinkertini', 'Shermell', 6, 4);
