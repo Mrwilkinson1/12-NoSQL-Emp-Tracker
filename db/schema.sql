64bb1bc23eac3c5fa74e3e77d56a1aa082dabdd3
Alex Wilkinson <mrwilkinson1@gmail.com>
2022-08-24 09:27:19 -0400

    push

--- db/schema.sql
+++ db/schema.sql
@@ -18,10 +18,11 @@ FOREIGN KEY (department_id) REFERENCES department(id)
 
 CREATE TABLE employee (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-last_name VARCHAR(30),
-first_name VARCHAR(30),
+last_name VARCHAR(30) NOT NULL,
+first_name VARCHAR(30) NOT NULL,
 manager_id INT,
 role_id INT,
 FOREIGN KEY (role_id) REFERENCES role(id),
-  FOREIGN KEY (manager_id) REFERENCES employee(id)
+  FOREIGN KEY (manager_id) REFERENCES employee(id) 
+  ON DELETE SET NULL
 );
--- server.js
+++ server.js
@@ -1,3 +1,16 @@
-const DB = require('espress')
-const mqsql = require ('mysql2')
+const DB = require('express')
+const mqsql = require('mysql2')
 const inquirer = require('inquirer ')
+
+
+// Database connection
+const DB = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'swavor',
+    database: 'Employee DB'
+},
+
+
+const PORT = process.env.PORT || 3001;
+const app = express();
